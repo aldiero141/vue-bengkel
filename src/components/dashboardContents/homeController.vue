35c02189f67f3baba4ece6fb3d93fa4473bf5538
= <yanuariush@gmail.com>
2019-11-22 17:57:28 +0700

    home controller.vue

--- src/components/dashboardContents/homeController.vue
+++ src/components/dashboardContents/homeController.vue
@@ -1,8 +1,36 @@
 <template>
-  
+    <v-container>
+        <h1 class="text-md-left">WELCOME!</h1>
+        <v-card>
+            <v-card>
+                <v-card-title class="headline">Service</v-card-title>
+                <v-card-subtitle>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </v-card-subtitle>
+            </v-card>
+        </v-card>
+        <br>
+        <v-card>
+            <v-card>
+                <v-card-title class="headline">Sparepart</v-card-title>
+                <v-card-subtitle>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </v-card-subtitle>
+            </v-card>
+        </v-card>
+        <br>
+        <v-card>
+            <v-card>
+                <v-card-title class="headline">Towing</v-card-title>
+                <v-card-subtitle>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </v-card-subtitle>
+            </v-card>
+        </v-card>        
+    </v-container>  
 </template>
 <script>
-export default {
-  
-}
+    export default {
+    
+  }
 </script>
